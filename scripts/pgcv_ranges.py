7a9ccb2c1874caa403927b54ed5fc2125be8494c
xyan43 <voidpointeryan@gmail.com>
2019-10-16 21:15:30 -0400

    minor

--- scripts/pgcv_ranges.py
+++ scripts/pgcv_ranges.py
@@ -5,15 +5,13 @@ range_common = [
     # [['seed'], [x for x in range(4)]],
     [['seed'], [0]],
     # [['exp_name'], ['unbiasedwithev']],
-    [['mdp', 'n_processes'], [8]],
-    [['experimenter', 'rollout_kwargs_pretrain', 'min_n_samples'], [50000]],
-    [['experimenter', 'rollout_kwargs_pretrain', 'max_n_rollouts'], [None]],
-    [['algorithm', 'delta'], [0.99]],
+    [['mdp', 'n_processes'], [16]],
+    [['experimenter', 'rollout_kwargs_pretrain', 'min_n_samples'], [10000]],
 ]
 
 range_comparison = [
     [['experimenter', 'rollout_kwargs', 'min_n_samples'], [None]],
-    [['experimenter', 'rollout_kwargs', 'max_n_rollouts'], [10]],
+    [['experimenter', 'rollout_kwargs', 'max_n_rollouts'], [5]],
 ]
 
 range_cv = [
@@ -21,7 +19,7 @@ range_cv = [
     [['algorithm', 'or_kwargs', 'cv_decay'], [.9]],
     [['algorithm', 'train_vf_with_sim'], [True]],
     [['algorithm', 'vfn_ro_kwargs', 'min_n_samples'], [50000]],
-    [['vfn_mdp_inacc'], [0.1]],   # biased
+    [['vfn_mdp_inacc'], [0.0]],   # biased
 ]
 
 range_upper_bound = range_common + [
